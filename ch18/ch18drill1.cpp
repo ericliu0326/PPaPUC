b89d3f348b44803ff175721bbfe19186109bdbae
Eric Liu <liu.p.eric@gmail.com>
2020-07-04 02:22:45 -0700

    ch 18 drill pt 1

---
+++ ch18/ch18drill1.cpp
@@ -0,0 +1,9 @@
+//
+//  ch18drill1.cpp
+//  C++
+//
+//  Created by Eric Liu on 7/3/20.
+//  Copyright © 2020 Eric Liu. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ ch18/ch18drill2.cpp
@@ -0,0 +1,9 @@
+//
+//  ch18drill2.cpp
+//  C++
+//
+//  Created by Eric Liu on 7/4/20.
+//  Copyright © 2020 Eric Liu. All rights reserved.
+//
+
+#include <stdio.h>
